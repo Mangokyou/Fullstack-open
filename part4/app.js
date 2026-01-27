aa044c1b0ccc9127adad5f61de439c2d08990c6e
Mangokyou <52341630+Mangokyou@users.noreply.github.com>
2026-01-27 10:33:20 +0100

    add part 4

---
+++ part4/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI, {family: 4})
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+
+
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
---
+++ part4/controllers/blogs.js
@@ -0,0 +1,38 @@
+const blogsRouter = require('express').Router()
+const Blog = require ('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get('/:id', (request, response) => {
+  Blog.findById(request.params.id)
+  .then(blog => {
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const body = request.body
+
+  const blog = new Blog ({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: 0
+  })
+
+  blog.save()
+  .then(savedBlog => {
+    response.json(savedBlog)
+  })
+})
+
+
+module.exports = blogsRouter
